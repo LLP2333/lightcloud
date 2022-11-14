fd489cdf0851d8d5afcb87251e50e3f73ae1fa26
llp <llp2333@qq.com>
2022-11-14 17:59:22 +0800

    新增了文件删除功能（软删除）

--- src/main/java/com/llp/lightcloud/controller/DeleteFileController.java
+++ src/main/java/com/llp/lightcloud/controller/DeleteFileController.java
@@ -1,9 +1,12 @@
 package com.llp.lightcloud.controller;
 
+import com.llp.lightcloud.service.UserFilesService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author llp
@@ -12,10 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @Slf4j
 public class DeleteFileController {
+    @Autowired
+    UserFilesService userFilesService;
     @RequestMapping("/delete/{id}")
+    @ResponseBody
     public String deleteFile(@PathVariable String id)
     {
-        return null;
+        boolean result = userFilesService.removeById(id);
+        if(result) return "succeed";
+        return "fail to delete the file";
+
     }
 
 }
